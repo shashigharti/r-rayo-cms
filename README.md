07836ca84f6e99ea41e3cf44efe0bc4f8c0e1604
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-07-22 10:03:56 +0545

    Added files webpack config package.json

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# react-core
-Core module for react
+# Core Module
+##Getting Started
+npm install
+npm run build
+
+##Conventions for React
+https://github.com/airbnb/javascript/tree/master/react#naming
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+module.exports = {
+    entry: './resources/js/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'js/app.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+            test: /\.(s*)css$/,
+            use: [
+              {
+                loader: "style-loader" // creates style nodes from JS strings
+              },
+              {
+                loader: "css-loader" // translates CSS into CommonJS
+              },
+              {
+                loader: "sass-loader" // compiles Sass to CSS
+              }
+            ]
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i, 
+          loader: "file-loader?name=/images/[name].[ext]"
+        }
+      ]
+    },
+    devtool: "cheap-module-eval-source-map",
+    externals: {
+      // global app config object
+      config: JSON.stringify({
+          apiUrl: 'http://www.lgprofile.local/'
+      })
+    }
+  };

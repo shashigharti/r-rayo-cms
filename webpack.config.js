34e10d1bf42028e8307d4dc8e76f83648f9b6aef
Arun Tamang <arun.tamang@amniltech.com>
2019-08-04 11:03:39 +0545

    Correct entry for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/App.js",
+    entry: "./src/index.js",
     mode: 'development',
     output: {
         path: path.resolve(__dirname, './dist'),

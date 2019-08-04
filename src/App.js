95f9446933bc0be582ace92e01c3ea7a8e06905c
Arun <aruntamang3791@gmail.com>
2019-08-04 18:12:19 +0545

    Configure eslint and prettier.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {HomePage} from './containers/home/Dashboard';
-import {LoginPage} from './containers/login/LoginPage';
-import {RegisterPage} from './containers/register/RegisterPage';
-import { ForgotPasswordPage } from './containers/forgot-password/ForgotPasswordPage';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 // import materialize
-import M from "materialize-css";
-import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+
+import { HomePage } from './containers/home/Dashboard';
+import { LoginPage } from './containers/login/LoginPage';
+import { RegisterPage } from './containers/register/RegisterPage';
+import { ForgotPasswordPage } from './containers/forgot-password/ForgotPasswordPage';
 
 import '../assets/css/themes/cms/materialize.css';
 import '../assets/css/themes/cms/style.css';
 import '../assets/css/components/color.css';
 
-import {PrivateRoute} from "./components";
+import { PrivateRoute } from './components';
 import * as serviceWorker from './serviceWorker';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        const {dispatch} = this.props;
-    }
-
     componentDidMount() {
         M.AutoInit();
 
@@ -30,26 +27,23 @@ class App extends React.Component {
     }
 
     render() {
-        const {alert} = this.props;
         return (
-            <div>
-                <Router>
-                    <PrivateRoute exact path="/" component={HomePage}/>
-                    <Route path="/login" component={LoginPage}/>
-                    <Route path="/register" component={RegisterPage}/>
-                    <Route path="/forgot-password" component={ForgotPasswordPage}/>
-                </Router>
-            </div>
+            <Router>
+                <PrivateRoute exact path="/" component={HomePage} />
+                <Route path="/login" component={LoginPage} />
+                <Route path="/register" component={RegisterPage} />
+                <Route path="/forgot-password" component={ForgotPasswordPage} />
+            </Router>
         );
     }
 }
 
 function mapStateToProps(state) {
-    const {alert} = state;
+    const { alert } = state;
     return {
-        alert
+        alert,
     };
 }
 
 const connectedApp = connect(mapStateToProps)(App);
-export {connectedApp as App};
+export { connectedApp as App };

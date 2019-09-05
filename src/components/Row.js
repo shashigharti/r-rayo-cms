9b2f3b7506f5458d7d6a2fa03d2bb17760702707
Bhumika <ganeshbhumika876@gmail.com>
2019-09-05 13:06:01 +0545

    btn-fix

--- src/components/Row.js
+++ src/components/Row.js
@@ -7,15 +7,12 @@ const Row = props => (
     <td className="text right-align">
       <a className="waves-effect waves-light btn-small cyan" onClick={() => console.log('wowowo')}>
         <i className="material-icons left">edit</i>
-        Edit
       </a>
       <a className="waves-effect waves-light btn-small amber">
         <i className="material-icons left">delete</i>
-        Delete
       </a>
       <a className="waves-effect waves-light btn-small purple">
         <i className="material-icons">check</i>
-        Enabled
       </a>
     </td>
   </tr>

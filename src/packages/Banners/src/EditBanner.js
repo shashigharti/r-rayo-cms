09fdf4779bf8647ced42b2fc256e662383a21c4f
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-21 08:22:59 +0545

    implemeneted dynamic code

--- src/packages/Banners/src/EditBanner.js
+++ src/packages/Banners/src/EditBanner.js
@@ -5,156 +5,179 @@ import { BannerContext } from '..';
 import { apiService, alertService } from '../../Core';
 import { EditResource } from '../../Core/Components/CRUD';
 import * as BannerTemplates from './../src/templates';
+import { Redirect } from 'react-router-dom';
 
-const EditBanner = (props) => {
-    const { dispatch: pdispatch } = useContext(BannerContext);
-    const [state, setState] = useState({
-        id: '',
-        name: '',
-        slug: '',
-        banner_template: 'FullScreenAd',
-        area_types: '',
-        sub_areas: '',
-        property_ids: '',
-        header: '',
-        content: '',
-        button_text: '',
-        button_url: ''
+const EditBanner = props => {
+  const { dispatch: pdispatch } = useContext(BannerContext);
+  const [toList, setToList] = useState(false);
+  const [state, setState] = useState({
+    id: '',
+    name: '',
+    slug: '',
+    banner_template: 'FullScreenAd',
+    status: '',
+    order: '',
+    area_types: '',
+    sub_areas: '',
+    property_ids: '',
+    header: '',
+    content: '',
+    button_text: '',
+    button_url: '',
+  });
+
+  useEffect(() => {
+    M.AutoInit();
+  }, [state]);
+
+  useEffect(() => {
+    console.log(props);
+    setState({
+      id: props.payload.id || '',
+      name: props.payload.name || '',
+      slug: props.payload.slug || '',
+      status: props.payload.status || '',
+      order: props.payload.order,
+      banner_template: props.payload.banner_template || 'FullScreenAd',
+      area_type: props.payload.area_type,
+      sub_areas: props.payload.sub_areas,
+      property_ids: props.payload.property_ids,
+      header: props.payload.header,
+      content: props.payload.content,
+      button_text: props.payload.button_text,
+      button_url: props.payload.button_url,
     });
+  }, [props]);
 
-    useEffect(() => {
-        M.AutoInit();
-        M.updateTextFields();
-    }, [state]);
+  useEffect(() => {
+    M.updateTextFields();
+  });
+  const handleSubmit = e => {
+    event.preventDefault();
+    const { id } = state;
+    const response = apiService.update(constants.API_BANNER_UPDATE + id, state.id);
+    const process = alertService.update(response);
+    process.then(status => {
+      if (status === true) {
+        pdispatch({ type: 'RESET' });
+        setTimeout(() => setToList(true), 500);
+      }
+    });
+  };
 
-    useEffect(() => {
-        setState({
-            id: props.payload.id || '',
-            name: props.payload.name || '',
-            slug: props.payload.slug || '',
-            banner_template: 'FullScreenAd',
-            area_type: '',
-            sub_areas: '',
-            property_ids: '',
-            header: '',
-            content: '',
-            button_text: '',
-            button_url: ''
-        });
-    }, [props]);
+  const renderSelectedTemplate = selectedTemplate => {
+    const BannerTemplate = BannerTemplates[selectedTemplate];
+    console.log(BannerTemplate);
+    return <BannerTemplate setFieldValue={setFieldValue} state={state} />;
+  };
 
-    const handleSubmit = e => {
-        event.preventDefault();
-        const updatePromise = apiService.update(constants.API_BANNER_UPDATE + id, state.id);
+  const setFieldValue = (field, value) => {
+    setState({
+      ...state,
+      [field]: value,
+    });
+    pdispatch({ type: 'SET_FIELD', current_page: { field, value } });
+  };
 
-        updatePromise.then(response => {
-            alertService.update(response.data);
-        });
-    };
-
-    const renderSelectedTemplate = selectedTemplate => {
-        const BannerTemplate = BannerTemplates[selectedTemplate];
-        console.log(BannerTemplate);
-        return (
-            <BannerTemplate setFieldValue={setFieldValue} />
-        );
-    }
-
-    const setFieldValue = (field, value) => {
-        setState({
-            ...state,
-            [field]: value,
-        });
-        pdispatch({ type: 'SET_FIELD', current_page: { field, value } });
-    };
-
-    return (
-        <div id="main">
-            <ToolBar breadcrumbs={constants.BREADCRUMB_BANNER_EDIT} toolbar={constants.TOOLBAR} />
-            <form onSubmit={handleSubmit}>
+  return (
+    <>
+      {toList ? <Redirect to={constants.BANNER} /> : null}
+      <div id="main">
+        <ToolBar breadcrumbs={constants.BREADCRUMB_BANNER_EDIT} toolbar={constants.TOOLBAR} />
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col s12">
+              <div className="container-fluid edit--page">
                 <div className="row">
-                    <div className="col s12">
-                        <div className="container-fluid">
-                            <div className="row">
-                                <div className="col s12">
-                                    <ul className="tabs">
-                                        <li className="tab">
-                                            <a className="active" href="#pages">
-                                                Banners
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="col s12">
-                                    <div className="panel card tab--content">
-                                        <div id="pages" className="col s12">
-                                            <div className="row">
-                                                <div className="input-field col s6">
-                                                    <label>Name</label>
-                                                    <input
-                                                        type="text"
-                                                        name="name"
-                                                        value={state.name}
-                                                        onChange={e => setFieldValue('name', e.target.value)}
-                                                        required
-                                                    />
-                                                </div>
-                                                <div className="input-field col s6">
-                                                    <label>Slug</label>
-                                                    <input
-                                                        type="text"
-                                                        name="slug"
-                                                        value={state.slug}
-                                                        onChange={e => setFieldValue('slug', e.target.value)}
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="row">
-                                                <div className="input-field col s12">
-                                                    <label>Templates</label>
-                                                    <select
-                                                        value={state.banner_template}
-                                                        name="banner_template"
-                                                        onChange={(e) => setFieldValue('banner_template', e.target.value)}
-                                                    >
-                                                        <option value="" disabled>
-                                                            Choose your option
-                                                        </option>
-                                                        <option value="TwoColumnAd">Two Column Ad</option>
-                                                        <option value="FullScreenAd">Full Screen Ad</option>
-                                                        <option value="Slider">Slider</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            {state.component}
-                                            <div className="row">
-                                                <div className="input-field col s6">
-                                                    <label>Status</label>
-                                                    <input
-                                                        type="text"
-                                                        name="status"
-                                                        value={state.status}
-                                                        onChange={e => setFieldValue('status', e.target.value)}
-                                                        required
-                                                    />
-                                                </div>
-                                            </div>
+                  <div className="col s12">
+                    <ul className="tabs">
+                      <li className="tab">
+                        <a className="active" href="#pages">
+                          Banners
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="col s12">
+                    <div className="panel card tab--content">
+                      <div id="pages" className="col s12">
+                        <div className="row">
+                          <div className="input-field col s6">
+                            <label>Name</label>
+                            <input
+                              type="text"
+                              name="name"
+                              value={state.name}
+                              onChange={e => setFieldValue('name', e.target.value)}
+                              required
+                            />
+                          </div>
+                          <div className="input-field col s6">
+                            <label>Slug</label>
+                            <input
+                              type="text"
+                              name="slug"
+                              value={state.slug}
+                              onChange={e => setFieldValue('slug', e.target.value)}
+                            />
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="input-field col s12">
+                            <label>Templates</label>
+                            <select
+                              defaultValue={state.banner_template}
+                              name="banner_template"
+                              onChange={e => setFieldValue('banner_template', e.target.value)}
+                            >
+                              <option value="" disabled>
+                                Choose your option
+                              </option>
+                              <option value="TwoColumnAd">Two Column Ad</option>
+                              <option value="FullScreenAd">Full Screen Ad</option>
+                              <option value="Slider">Slider</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="input-field col s6">
+                            <label>Status</label>
+                            <select
+                              defaultValue={state.status}
+                              name="status"
+                              onChange={e => setFieldValue('status', e.target.value)}
+                            >
+                              <option value="" disabled>
+                                Choose your option
+                              </option>
+                              <option value="0">Active</option>
+                              <option value="1">Inactive</option>
+                            </select>
+                          </div>
+                          <div className="input-field col s6">
+                            <label>Order</label>
+                            <input
+                              type="text"
+                              name="order"
+                              value={state.order}
+                              onChange={e => setFieldValue('order', e.target.value)}
+                            />
+                          </div>
+                        </div>
 
-                                            {renderSelectedTemplate(state.banner_template)}
+                        {renderSelectedTemplate(state.banner_template)}
 
-                                            <div className="row">
-                                                <div className="col s12">
-                                                    <div className="input-field">
-                                                        <button type="submit" className="btn gradient-45deg-purple-deep-orange">
-                                                            Submit
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div className="row">
+                          <div className="col s12">
+                            <div className="input-field">
+                              <button
+                                type="submit"
+                                className="btn gradient-45deg-purple-deep-orange"
+                              >
+                                Submit
+                              </button>
                             </div>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -163,9 +186,9 @@ const EditBanner = (props) => {
               </div>
             </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 };
 
--- src/packages/Banners/src/templates/slider.js
+++ src/packages/Banners/src/templates/slider.js
@@ -1,61 +1,67 @@
-import React from 'react';
-export default (props) => {
-    const { setFieldValue } = props;
+import React, { useEffect } from 'react';
+export default props => {
+  useEffect(() => {
+    M.AutoInit();
+  });
+  useEffect(() => {
+    M.updateTextFields();
+  });
+  const { setFieldValue, state } = props;
 
-    return (
-        <div className="banner-template">
-            <div className="row">
-                <div className="input-field col s12">
-                    <label>Areas</label>
-                    <select
-                        value={props.area_types}
-                        name="area_types"
-                        onChange={(e) => setFieldValue('area_types', e.target.value)}
-                        multiple
-                    >
-                        <option value="" disabled>
-                            Choose your option
-                        </option>
-                        <option value="cities">Cities</option>
-                        <option value="counties">Counties</option>
-                        <option value="school">High School</option>
-                    </select>
-                </div>
-            </div>
-            <div className="row">
-                <div className="input-field col s12">
-                    <label>Cities</label>
-                    <select
-                        value={props.sub_areas}
-                        name="sub_areas"
-                        onChange={(e) => setFieldValue('sub_areas', e.target.value)}
-                    >
-                        <option value="" disabled>
-                            Choose your option
-                        </option>
-                        <option value="1">City 1</option>
-                        <option value="2">City 2</option>
-                        <option value="3">City 3</option>
-                    </select>
-                </div>
-            </div>
-            <div className="row">
-                <div className="input-field col s12">
-                    <label>Items</label>
-                    <select
-                        value={props.property_ids}
-                        name="property_ids"
-                        onChange={(e) => setFieldValue('property_ids', e.target.value)}
-                    >
-                        <option value="" disabled>
-                            Choose your option
-                        </option>
-                        <option value="1">Property 1</option>
-                        <option value="2">Property 2</option>
-                        <option value="3">Property 3</option>
-                    </select>
-                </div>
-            </div>
+  return (
+    <div className="banner-template">
+      <div className="row">
+        <div className="input-field col s12">
+          <label>Areas</label>
+          <select
+            defaultValue={state.area_types}
+            name="area_types"
+            onChange={e => setFieldValue('area_types', e.target.value)}
+            multiple
+          >
+            <option value="" disabled>
+              Choose your option
+            </option>
+            <option value="cities">Cities</option>
+            <option value="counties">Counties</option>
+            <option value="school">High School</option>
+          </select>
         </div>
-    );
-}
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <label>Cities</label>
+          <select
+            defaultValue={state.sub_areas}
+            name="sub_areas"
+            onChange={e => setFieldValue('sub_areas', e.target.value)}
+          >
+            <option value="" disabled>
+              Choose your option
+            </option>
+            <option value="1">City 1</option>
+            <option value="2">City 2</option>
+            <option value="3">City 3</option>
+          </select>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <label>Items</label>
+          <select
+            defaultValue={state.property_ids}
+            name="property_ids"
+            onChange={e => setFieldValue('property_ids', e.target.value)}
+          >
+            <option value="" disabled>
+              Choose your option
+            </option>
+            <option value="1">Property 1</option>
+            <option value="2">Property 2</option>
+            <option value="3">Property 3</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};

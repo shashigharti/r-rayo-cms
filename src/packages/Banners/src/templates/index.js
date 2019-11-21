c64ec010241b3bfce86e04f52b9d05a340ecf5d7
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-21 10:47:52 +0545

    import fix

--- src/packages/Banners/src/templates/index.js
+++ src/packages/Banners/src/templates/index.js
@@ -1,6 +1,6 @@
 import FullScreenAd from './full-screen-ad';
 import Slider from './slider';
 import TwoColumnAd from './two-col-ad';
-import Block from './single-column-block';
+import SingleColumnBlock from './single-column-block';
 
 export { FullScreenAd, Slider, TwoColumnAd, SingleColumnBlock };
